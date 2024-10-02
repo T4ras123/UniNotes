04727c01241cd485d8550ea749dc0fc962c165a3
T4ras123 <vovatara123@gmail.com>
2024-10-02 14:46:31 +0400

    created utils file for common functions

---
+++ LaTex-TrOCR/utils/utils.py
@@ -0,0 +1,14 @@
+import torch
+import random 
+import os
+
+def exist(val):
+    return val is not None
+
+def get_device(args, no_cuda=False):
+    device = 'cpu'
+    available_gpus = torch.cuda.device_count()
+    args.gpu_devices = args.gpu_devices if args.get('gpu_devices', False) else list(range(available_gpus))
+    if available_gpus > 0 and not no_cuda:
+        device = 'cuda:%d' % args.gpu_devices[0] if args.gpu_devices else 0
+    return device
