07b22acc1dca7b47542d2c9aaa5cc174cc07958d
T4ras123 <vovatara123@gmail.com>
2024-10-04 21:58:56 +0400

    docs(parquet): added a function discription

--- LaTeXTrOCR/dataset/parquet.py
+++ LaTeXTrOCR/dataset/parquet.py
@@ -10,6 +10,15 @@ import pandas as pd
 ds = pd.DataFrame()
 
 def parquet_to_tuple(path=None) -> pd.DataFrame:
+    """Convert parquet file to pandas dataframe 
+    and convert bytes to a string of pixel values (H,W,C)
+
+    Args:
+        path (str, optional): path to the parquet file. Defaults to None.
+
+    Returns:
+        pd.DataFrame: str of pixel values in the "image" column and LaTex in the "text" one
+    """
     ds = pd.DataFrame(columns=['image', 'text'])
     if path is not None:
         dataset = pd.read_parquet(path)
