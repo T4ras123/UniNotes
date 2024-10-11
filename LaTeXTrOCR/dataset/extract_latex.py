9bdb7983e9b7c772d00c815234d4beb8cc01991d
T4ras123 <vovatara123@gmail.com>
2024-10-11 18:06:01 +0400

    feat(extract_latex.py): add argument parsing

--- LaTeXTrOCR/dataset/extract_latex.py
+++ LaTeXTrOCR/dataset/extract_latex.py
@@ -9,13 +9,11 @@ MAX_CHARS = 5000
 
 
 
-def check_brackets(string):
-    
+def check_brackets(string):    
     a = []
     surrounding = False
 
     for i, ch in enumerate(string):
-        
         if ch == '{':
             if i>0 and string[i-1]=='\\':
                 continue
@@ -23,7 +21,6 @@ def check_brackets(string):
                 a.append(1)
             if i == 0:
                 surrounding=True
-
         elif ch=='}':
             if i>0 and string[i-1]=='\\':
                 continue
@@ -31,9 +28,12 @@ def check_brackets(string):
                 a.append(-1)
           
     b = sum(a)
+
     if len(a) >= 1 and b != 0:
         raise ValueError(string)
+
     surrounding = (surrounding and string[-1]=='}')
+
     if not surrounding:
         return string
     elif (b == 0) == 1:
@@ -42,7 +42,13 @@ def check_brackets(string):
         return string
 
 
-print(check_brackets("ofhowgo\\{ wkfm"))
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(dest='file', type=str, help="file to find equations in")
+    parser.add_argument('--out', '-o', type=str, default=None, help="file to save equations to. If unspecified all equations are printed")
+    parser.add_argument('--wiki', '-w', action='store_true', help="only look for math starting with \\displaystyle")
+    args = parser.parse_args()
 
-            
-            
+    if not os.path.exists(args.file):
+        raise ValueError('File can not be found. %s' % args.file)
+    
